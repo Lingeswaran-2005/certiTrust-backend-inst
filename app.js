a577ae107228ae2d7aa8171697e8382e7a8b97da
Lingeswaran-2005 <lingeswaran2195@gmail.com>
2025-09-18 11:46:01 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const pool = require('./db')
+const PORT = 5000
+
+//MiddleWare
+
+app.use(cors())
+app.use(express.json())
+
+// Helper Functions
+
+async function getHashAndSign(tableName , code) {
+    const result = await pool.query(`select department_signature , institution_signature from ${tableName} where short_code = $1`,[code])
+    return(result.rows[0])
+}
+
+// Routing
+
+app.post("/rec/hash",async(req,res)=>{
+    const data = req.body
+    try {
+        
+        const hash = await getHashAndSign(data["deptTableName"],data["short_code"])
+        
+
+        if (!hash){
+            res.status(404).json({"message":"No Hash Found"})
+        }
+
+        
+        const {department_signature:deptSign  , institution_signature : instSign} = hash
+        
+
+        res.status(200).json({"message":"hash sent successfully","deptSign":deptSign,"instSign":instSign})
+
+    } catch (error) {
+
+        console.log(error)
+        res.status(500).json({"message":"DB Error"})
+
+    }
+})
+
+
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
