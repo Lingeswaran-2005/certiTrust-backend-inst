3f7ffd57779360804a3de88ab5aa4026ae3838e1
Lingeswaran-2005 <lingeswaran2195@gmail.com>
2025-09-19 14:44:20 +0530

    hashing route added successfully locally no db added

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const cors = require('cors')
 const pool = require('./db')
 const PORT = 5000
+const dotenv = require("dotenv").config()
+const crypto = require('crypto')
 
 //MiddleWare
 
@@ -14,6 +16,12 @@ app.use(express.json())
 async function getHashAndSign(tableName , code) {
     const result = await pool.query(`select department_signature , institution_signature from ${tableName} where short_code = $1`,[code])
     return(result.rows[0])
+}
+
+function generateSignature(data,privateKey){
+    const hash = crypto.createHash("sha256").update(data).digest()
+    const sign = crypto.createSign("sha256").update(hash).sign(privateKey , "base64")
+    return(sign)
 }
 
 // Routing
@@ -41,6 +49,42 @@ app.post("/rec/hash",async(req,res)=>{
         return res.status(500).json({"message":"DB Error"})
 
     }
+})
+
+app.post("/api/dept/hash",(req,res)=>{
+    try{
+        const {data}  = req.body
+        const rawData = data.map(i=>{
+            return `${i["name"]}|${i["dept"]}|${i["issuedate"]}`
+        })
+        
+        let deptSignatures = rawData.map(i=> generateSignature(i,process.env.DEPT_PRIVATE_KEY))
+        console.log(deptSignatures)
+        return res.status(200).json({message:"Department signature successfull"})
+    }
+    catch(err){
+        console.log(err.message)
+        res.status(500).json({message:"internal server error"})
+    }
+
+})
+
+app.post("/api/inst/hash",(req,res)=>{
+    try{
+        const {data}  = req.body
+        const rawData = data.map(i=>{
+            return `${i["name"]}|${i["dept"]}|${i["issuedate"]}`
+        })
+        
+        let deptSignatures = rawData.map(i=> generateSignature(i,process.env.INST_PRIVATE_KEY))
+        console.log(deptSignatures)
+        return res.status(200).json({message:"Institution signature successfull"})
+    }
+    catch(err){
+        console.log(err.message)
+        res.status(500).json({message:"internal server error"})
+    }
+
 })
 
 
